c2e9a0ec229c7f536f2fa19f5d2b3f0eb440502d
ValeriaRabch <rabchenukv@gmail.com>
2024-03-20 21:49:07 +0200

    Create file

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 
 int main() {
-	
+	FILE* f; char name[20];
+	char link[] = { "d:\\Valeria\\g.txt" };
+	f = fopen("d:\\Valeria\\F.txt", "r");
+	Lower(f, link);
+
+	fclose(f);
 	return 0;
 }
